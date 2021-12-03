84246f921f4442a3c4e09b5e07665de090030647
Zhu He <hezhu1688@gmail.com>
2021-12-03 22:25:54 +0800

    add manual trigger and remove base path

--- update.py
+++ update.py
@@ -3,7 +3,6 @@ import re
 import shutil
 from urllib.request import urlopen
 
-base_path = "data"
 base_url = "http://vrp.atd-lab.inf.puc-rio.br"
 index_path = "/index.php/en/"
 pattern = re.compile('"(/media/com_vrp/instances/(.*)/(.*\.(vrp|sol)))"')
@@ -11,9 +10,8 @@ pattern = re.compile('"(/media/com_vrp/instances/(.*)/(.*\.(vrp|sol)))"')
 with urlopen(base_url + index_path) as response:
     body = response.read().decode()
 
-for path, set_name, file_name, _ in pattern.findall(body):
+for path, dir_path, file_name, _ in pattern.findall(body):
     print(path)
-    dir_path = os.path.join(base_path, set_name)
     file_path = os.path.join(dir_path, file_name)
     os.makedirs(dir_path, exist_ok=True)
     with urlopen(base_url + path, timeout=10) as response:
